43cb1153fb2f9a703269d70fb9d38cd306af9794
Pandhuta Bangkit S <bangkitsamesta@gmail.com>
2022-11-01 12:35:40 +0700

    FutureASub

--- amartha.java
+++ amartha.java
@@ -3,5 +3,6 @@ public class amartha {
     public static void main(String[] args) {
         System.out.println("Hello World yeah");
         System.out.println("Future A");
+        System.out.println("FutureASub");
     }
 }
